bb650ac90725dffe3231a85c008ad0dfd13a91df
vedant <vedantdalvi05@gmail.com>
2023-04-08 12:23:12 +0530

    Player control update
    Added some player controls, environment assets.

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -9,6 +9,7 @@ extends CharacterBody3D
 @onready var animation_player : Object = $AnimationPlayer
 @onready var player_sprite : Object = $PlayerSprite
 @onready var camera : Object = $Camera3D
+@onready var state_machine = $AnimationTree.get("parameters/playback")
 
 var vel : Vector3 = Vector3.ZERO
 
@@ -18,27 +19,26 @@ func get_input(delta):
 	var direction : Vector3 = Vector3.ZERO
 
 	direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-
-	if direction.x == 0 and !Input.is_action_just_pressed("fire"):
-		animation_player.play("idle")
-	elif direction.x > 0:
-		player_sprite.flip_h = false
-	elif direction.x < 0:
-		player_sprite.flip_h = true
-
 	if direction.x != 0:
+		if direction.x < 0:
+			player_sprite.flip_h = true
+		elif direction.x > 0:
+			player_sprite.flip_h = false
+		
 		if Input.is_action_pressed("run"):
-			animation_player.play("run")
+			state_machine.travel("run")
 			speed = 5
 		else:
-			animation_player.play("walk")
+			state_machine.travel("walk")
 			speed = 2
-	elif Input.is_action_just_pressed("fire"):
-		animation_player.play("fire")
-		await get_tree().create_timer(0.5).timeout
+	else:
+		state_machine.travel("idle")
+	
+	
+	if Input.is_action_just_pressed("fire"):
+		state_machine.travel("fire")
 
 	direction = direction.normalized()
-	print(direction)
 
 	velocity = velocity.lerp(direction * speed, acceleration * delta)
 	print(velocity)
@@ -47,9 +47,3 @@ func get_input(delta):
 func _physics_process(delta):
 	get_input(delta)
 	move_and_slide()
-	
-	
-#	var dir = Input.get_axis("move_left", "move_right")
-#
-#	if dir != 0:
-#		velocity.x = lerp(velocity.x, dir * spe)
