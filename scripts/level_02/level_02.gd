e99b79599cc3b7c409ce2b98d94e51871d00a35d
vedant <vedantdalvi05@gmail.com>
2023-04-20 15:12:59 +0530

    minor level_02_updates

--- scripts/level_02/level_02.gd
+++ scripts/level_02/level_02.gd
@@ -5,15 +5,15 @@ extends Node3D
 @onready var music : AudioStream = preload("res://sounds/just_evil(cyberpunk).mp3")
 @onready var audio_player : AudioStreamPlayer = $AudioPlayer
 
+@onready var player : Node3D = $Player
+@onready var player_2 : Node3D = $Player2
+@onready var player_animation : AnimationPlayer = $Player/AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export var player_speed : int = 2
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	player.global_position.x += player_speed * delta
 
 
 func _on_animation_player_animation_finished(anim_name):
@@ -22,3 +22,15 @@ func _on_animation_player_animation_finished(anim_name):
 		animation_player.play("establishing_shot")
 #		audio_player.set_stream(music)
 #		audio_player.play()
+	elif anim_name == "fade_out":
+		player.hide()
+		player_2.show()
+		animation_player.play("fade_in_2")
+
+
+func _on_hotel_area_area_entered(area):
+	if area.name == "PlayerArea":
+		player_speed = 0
+		player_animation.play("idle")
+		$HotelArea.queue_free()
+		animation_player.play("fade_out")
